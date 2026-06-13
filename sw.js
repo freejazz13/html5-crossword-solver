b08e86e7f832846c37c72f49e9cdeae99d72693c
Jerome <me@hp.jer>
2026-06-13 18:41:00 +0200

    dev test precommit2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // CRITICAL: You MUST change this string (e.g., v3, v4) every time you deploy a new update!
-const CACHE_NAME = "xw-solver-v6";
+const CACHE_NAME = "nexplay-2026-06-13-750706";
 
 const ASSETS = [
   "/",
